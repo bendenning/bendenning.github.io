69bf7deea07e073fa701cf67f81115ca4a0d17d0
bendenning <45517878+bendenning@users.noreply.github.com>
2019-12-17 20:42:45 +0000

    Add files via upload

--- assets/gallery/script.js
+++ assets/gallery/script.js
@@ -25,6 +25,8 @@
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
         var players = [];
 
+        $('.carousel-item.video-container img').css('display','none');
+
         /* google iframe api init function */
         window.onYouTubeIframeAPIReady = function() {
             var ytp = ytp || {};
@@ -93,18 +95,16 @@
 
     /* Masonry Grid */
     $(document).on('add.cards', function(event) {
-        var $section = $(event.target),
-            allItem = $section.find('.mbr-gallery-filter-all');
-        var filterList = [];
-        $section.on('click', '.mbr-gallery-filter li > .btn', function(e) {
+        var $section = $(event.target);
+        $section.on('click', '.mbr-gallery-filter li', function(e) {
             e.preventDefault();
             var $li = $(this).closest('li');
 
-            $li.parent().find('a').removeClass('active');
-            $(this).addClass('active');
+            $li.parent().find('li').removeClass('active');
+            $li.addClass('active');
 
             var $mas = $li.closest('section').find('.mbr-gallery-row');
-            var filter = $(this).html().trim();
+            var filter = $(this)[0].textContent.trim();
 
             $section.find('.mbr-gallery-item').each(function(i, el) {
                 var $elem = $(this);
@@ -118,115 +118,169 @@
                 if ($.inArray(filter, tagsTrimmed) === -1 && !$li.hasClass('mbr-gallery-filter-all')) {
                     $elem.addClass('mbr-gallery-item__hided');
 
-                    setTimeout(function() {
-                        $elem.css('left', '300px');
-                    }, 200);
+                    $elem.css('left', '300px');
                 } else {
                     $elem.removeClass('mbr-gallery-item__hided');
                 }
             });
 
-            setTimeout(function() {
-                $mas.closest('.mbr-gallery-row').trigger('filter');
-            }, 50);
+            $mas.closest('.mbr-gallery-row').trigger('filter');
         });
-    })
-    $(document).on('add.cards changeParameter.cards changeButtonColor.cards', function(event) {
-        var $section = $(event.target),
-            allItem = $section.find('.mbr-gallery-filter-all');
+    });
+
+    if (isBuilder) {
+        $(document).on('changeButtonColor.cards', function(event) {
+            var $section = $(event.target);
+
+            if ($section.find('.mbr-gallery-filter').length > 0 && $(event.target).find('.mbr-gallery-filter').hasClass('gallery-filter-active')) {
+                var classAttr = ($section.find('.mbr-gallery-filter .mbr-gallery-filter-all').find('a').attr('class') || '').replace(/(^|\s)active(\s|$)/, ' ').trim();
+
+                $section.find('.mbr-gallery-filter ul li:not(.mbr-gallery-filter-all) a').attr('class', classAttr);
+            }
+
+            updateMasonry(event);
+        });
+    }
+
+    $(document).on('add.cards changeParameter.cards', function(event) {
+        var $section = $(event.target);
         var filterList = [];
 
         $section.find('.mbr-gallery-item').each(function(el) {
-            var tagsAttr = ($(this).attr('data-tags') || "").trim();
-            var tagsList = tagsAttr.split(',');
+            var tagsList = ($(this).attr('data-tags') || "").trim().split(',');
 
             tagsList.map(function(el) {
-                var tag = el.trim();
-
-                if ($.inArray(tag, filterList) === -1 && tag.trim())
-                    filterList.push(tag);
+                el = el.trim();
+                if ($.inArray(el, filterList) === -1) filterList.push(el);
             });
         });
 
-        if ($section.find('.mbr-gallery-filter').length > 0 && $(event.target).find('.mbr-gallery-filter').hasClass('gallery-filter-active') && !$(event.target).find('.mbr-gallery-filter').hasClass('mbr-shop-filter')) {
+        if ($section.find('.mbr-gallery-filter').length > 0 && $(event.target).find('.mbr-gallery-filter').hasClass('gallery-filter-active')) {
             var filterHtml = '';
 
-            var classAttr = allItem.find('a').attr('class') || '';
-            classAttr = classAttr.replace(/(^|\s)active(\s|$)/, ' ').trim();
+            $section.find('.mbr-gallery-filter ul li:not(.mbr-gallery-filter-all)').remove();
 
-            $section.find('.mbr-gallery-filter ul li:not(li:eq(0))').remove();
-
+            var allItem = $section.find('.mbr-gallery-filter .mbr-gallery-filter-all'),
+                classAttr = (allItem.find('a').attr('class') || '').replace(/(^|\s)active(\s|$)/, ' ').trim();
+ 
             filterList.map(function(el) {
                 filterHtml += '<li><a class="' + classAttr + '" href>' + el + '</a></li>';
             });
+
             $section.find('.mbr-gallery-filter ul').append(filterHtml);
 
-        } else {
-            $section.find('.mbr-gallery-item__hided').removeClass('mbr-gallery-item__hided');
-            $section.find('.mbr-gallery-row').trigger('filter');
         }
 
         updateMasonry(event);
     });
 
-    $(document).on('change.cards sortstop', function(event) {
+    $(document).on('change.cards', function(event) {
         updateMasonry(event);
+    });
+
+    $(document).on('lazyload', function(event) {
+        updateMasonry(event);
+        $(window).scrollEnd(function(){
+            updateMasonry(event);
+        }, 250)
     });
 
     $('.mbr-gallery-item').on('click', 'a', function(e) {
         e.stopPropagation();
     });
 
-    var timeout;
-    var timeout2;
-
-    function fitLBtimeout() {
-        clearTimeout(timeout);
-        timeout = setTimeout(fitLightbox, 50);
-    }
+    var timeout2, timeout, objectLightBox;
 
     /* Lightbox Fit */
-    function fitLightbox() {
-        var $lightbox = $('.mbr-gallery .modal');
-        if (!$lightbox.length) {
-            return;
-        }
+    function styleVideo($carouselItem, wndH, windowPadding, bottomPadding){
+        $carouselItem.css({
+            'top': windowPadding,
+            'height': wndH - 2 * windowPadding - 2 * bottomPadding
+        })
+    }
 
+    function styleImg($carouselItem, wndH, wndW, windowPadding, bottomPadding){
+        var $currentImg = $carouselItem.find('img');
+        if ($currentImg[0].complete && $currentImg[0].naturalWidth>50 && $currentImg[0].naturalHeight>50) {
+            setCSStoImage($currentImg,$carouselItem, wndH, wndW, windowPadding, bottomPadding)
+        } else {
+            $currentImg.one('load', function() {
+                setCSStoImage($currentImg,$carouselItem, wndH, wndW, windowPadding, bottomPadding)
+            })
+        }
+    }
+
+    function setCSStoImage(image,item, wndH, wndW, windowPadding, bottomPadding) {
+        var setWidth, setTop;
+
+        var lbW = image[0].naturalWidth;
+        var lbH = image[0].naturalHeight;
+
+        // height change
+        if (wndW / wndH > lbW / lbH) {
+            var needH = wndH - bottomPadding * 2;
+            setWidth = needH * lbW / lbH;
+        } else { // width change
+            setWidth = wndW - bottomPadding * 2;
+        }
+        // check for maw width
+        setWidth = setWidth >= lbW ? lbW : setWidth;
+
+        // set top to vertical center
+        setTop = (wndH - setWidth * lbH / lbW) / 2;
+
+        image.css({
+            width: parseInt(setWidth),
+            height: setWidth * lbH / lbW
+        });
+        item.css('top', setTop + windowPadding);
+    }
+
+    function timeOutCarousel($lightBox, wndW, wndH, windowPadding, bottomPadding, flagResize){
+        var $carouselItems = $lightBox.find('.modal-dialog .carousel-item');
+
+        $carouselItems.each(function() {
+            if ((!flagResize && !$(this)[0].classList.contains('carousel-item-next') && !$(this)[0].classList.contains('carousel-item-prev')) || (flagResize && !$(this)[0].classList.contains('active'))){
+                if ($(this)[0].classList.contains('video-container')){
+                    styleVideo($(this), wndH, windowPadding, bottomPadding);
+                }
+                else{
+                    styleImg($(this), wndH, wndW, windowPadding, bottomPadding);
+                }
+            }
+        });
+    }
+
+    function fitLightbox() {
         var windowPadding = 0;
         var bottomPadding = 10;
         var wndW = $(window).width() - windowPadding * 2;
         var wndH = $(window).height() - windowPadding * 2;
 
-        $lightbox.each(function() {
-            var setWidth, setTop;
-            var $modalDialog = $(this).find('.modal-dialog');
-            var $currentImg = $modalDialog.find('.carousel-item.active > img');
+        if (!objectLightBox) {
+            return;
+        }
 
-            if ($modalDialog.find('.carousel-item.prev > img, .carousel-item.next > img').length) {
-                $currentImg = $modalDialog.find('.carousel-item.prev > img, .carousel-item.next > img').eq(0);
-            }
+        var $carouselItemActive, flagResize = false;
+        if (objectLightBox.find('.modal-dialog .carousel-item.carousel-item-next, .modal-dialog .carousel-item.carousel-item-prev').length){
+            $carouselItemActive = objectLightBox.find('.modal-dialog .carousel-item.carousel-item-next, .modal-dialog .carousel-item.carousel-item-prev');
+        }
+        else{
+            $carouselItemActive = objectLightBox.find('.modal-dialog .carousel-item.active');
+            flagResize = true;
+        }
 
-            var lbW = $currentImg[0].naturalWidth;
-            var lbH = $currentImg[0].naturalHeight;
+        if($carouselItemActive[0].classList.contains('video-container')){
+            styleVideo($carouselItemActive, wndH, windowPadding, bottomPadding);
+        }
+        else{
+            styleImg($carouselItemActive, wndH, wndW, windowPadding, bottomPadding);
+        }
 
-            // height change
-            if (wndW / wndH > lbW / lbH) {
-                var needH = wndH - bottomPadding * 2;
-                setWidth = needH * lbW / lbH;
-            } else { // width change
-                setWidth = wndW - bottomPadding * 2;
-            }
-            // check for maw width
-            setWidth = setWidth >= lbW ? lbW : setWidth;
+        clearTimeout(timeout);
 
-            // set top to vertical center
-            setTop = (wndH - setWidth * lbH / lbW) / 2;
+        timeout = setTimeout( timeOutCarousel, 200, objectLightBox, wndW, wndH, windowPadding, bottomPadding, flagResize);
 
-            $modalDialog.css({
-                width: parseInt(setWidth),
-                top: setTop + windowPadding
-            });
-        });
     }
 
     /* pause/start video on different events and fit lightbox */
@@ -235,7 +289,7 @@
     $window.on('show.bs.modal', function(e) {
         clearTimeout(timeout2);
 
-        var timeout2 = setTimeout(function() {
+        timeout2 = setTimeout(function() {
             var index = $(e.relatedTarget).parent().index();
             var slide = $(e.target).find('.carousel-item').eq(index).find('.mbr-background-video');
             $(e.target).find('.carousel-item .mbr-background-video');
@@ -245,7 +299,10 @@
             }
         }, 500);
 
-        fitLBtimeout();
+        objectLightBox = $(e.target);
+
+        fitLightbox();
+
     });
 
     $window.on('slide.bs.carousel', function(e) {
@@ -256,7 +313,7 @@
         }
     });
 
-    $(window).on('resize load', fitLBtimeout);
+    $(window).on('resize load', fitLightbox);
 
     $window.on('slid.bs.carousel', function(e) {
         var ytv = $(e.target).find('.carousel-item.active .mbr-background-video');
@@ -265,13 +322,14 @@
             var player = players[+ytv.attr('data-video-num')];
             player.playVideo ? player.playVideo() : player.play();
         }
-
-        fitLBtimeout();
+        
     });
 
     $window.on('hide.bs.modal', function(e) {
         players.map(function(player, i) {
             player.pauseVideo ? player.pauseVideo() : player.pause();
         });
+
+        objectLightBox = null;
     });
 }(jQuery));
